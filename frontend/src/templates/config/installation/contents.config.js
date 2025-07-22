9d0e9bbdcdf779ba8fc51d22db85408f820143f5
virenderpccvdi <virender.kumar@pccvdi.com>
2025-07-22 17:16:20 +0000

    Name Chnages

--- frontend/src/templates/config/installation/contents.config.js
+++ frontend/src/templates/config/installation/contents.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
     ],
     belowNavMenu: null,
-    footer: 'Powered by <a href="//demo.mediacms.io" title="mediacms.io" target="_blank">mediacms.io</a>',
+    footer: 'Powered by <a href="//pccvdi.com" title="Medix" target="_blank">PCCVDI Solutions </a>',
   },
   uploader: {
     belowUploadArea: '',
